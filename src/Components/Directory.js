f2598dd84901e98714d2721a1af0e82482a8afb7
Steve Munoz <stevomunoz@yahoo.com>
2020-09-11 20:41:41 -0500

    made comments to use axios for API in utils.js

--- src/Components/Directory.js
+++ src/Components/Directory.js
@@ -4,3 +4,15 @@
 // sort by first name
 // sort by last name
 // render
+
+
+import React, { Component } from 'react'
+import employees from "./db.json"
+
+
+export default class Directory extends Component {
+    state = {
+        employees: employees
+    }
+
+    
