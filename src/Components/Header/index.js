9081c3b35bda6a4be12c306089f9178f4636458e
Steve Munoz <stevomunoz@yahoo.com>
2020-09-13 22:47:24 -0500

    added css to header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,9 +1,26 @@
 import React from "react";
+import "./style.css";
 
 export default function index() {
   return (
-    <div style={{ textAlign: "center", color: "blue" }}>
-      <h1>Employee Directory</h1>
+    <h1
+      style={{
+        textAlign: "center",
+        color: "white",
+        backgroundColor: "black",
+        width: "100vw",
+        textAlign: "center",
+        borderBottom: "#e36414 4px solid",
+        zIndex: 1,
+        transform: "translate(-5-%, -50%)",
+      }}
+    >
+      Employee Directory
+    </h1>
+    <div>
+        <h6>
+            
+        </h6>
     </div>
   );
 }
