7af799a9661162d284bad9975a07a33d79e20fab
Steve Munoz <stevomunoz@yahoo.com>
2020-09-16 22:04:20 -0500

    used bootstrap for responsiveness and styling

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,26 +1,37 @@
 import React from "react";
 import "./style.css";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+
+const pStyle = { textAlign: "center", color: "red", display: "inline-block" };
 
 export default function index() {
   return (
-    <h1
-      style={{
-        textAlign: "center",
-        color: "white",
-        backgroundColor: "black",
-        width: "100vw",
-        textAlign: "center",
-        borderBottom: "#e36414 4px solid",
-        zIndex: 1,
-        transform: "translate(-5-%, -50%)",
-      }}
-    >
-      Employee Directory
-    </h1>
     <div>
-        <h6>
-            
-        </h6>
+      <Navbar bg="dark" variant="dark" fixed="top" style={pStyle}>
+        <Navbar.Brand>Employee Directory</Navbar.Brand>
+        <Nav className="mr-auto"></Nav>
+      </Navbar>
     </div>
   );
 }
+
+// return (
+
+//     <div>
+//       <h1
+//         style={{
+//           textAlign: "center",
+//           color: "white",
+//           backgroundColor: "black",
+//           width: "100vw",
+//           textAlign: "center",
+//           borderBottom: "#e36414 4px solid",
+//           zIndex: 1,
+//           transform: "translate(-5-%, -50%)",
+//         }}
+//       >
+//         Employee Directory
+//       </h1>
+//     </div>
+//   );
+// }
